f29becb9f1b6700f138cfe0d7901181a53ab3f17
Onni Ahopelto <onni.ahopelto@gmail.com>
2024-12-14 13:00:11 +0200

    Client game logic test

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,21 @@ interface Message {
   Typ: number;
   Data?: string;
   PlayerId?: number;
+  Card?: Card;
+  ScoreCards?: Card[];
+}
+
+interface Card {
+  Suit: string;
+  Value: string;
 }
 
 function App() {
   const [socketUrl, _] = useState<string>("ws://localhost:3000");
   const [isCurrentTurn, setIsCurrentTurn] = useState<boolean>(false);
   const [playerId, setPlayerId] = useState<number>(0);
+  const [cards, setCards] = useState<Card[]>([]);
+  const [scoreCards, setScoreCards] = useState<Card[]>([]);
   const [__, setMessageHistory] = useState<MessageEvent<any>[]>([]);
 
   const { lastMessage, sendMessage } = useWebSocket(socketUrl);
@@ -26,8 +35,13 @@ function App() {
             setPlayerId(data.PlayerId!);
           }
           break;
-        case 5: // Player1Turn
+        case 5: // Player1Turn + winning condition
+          if (data.ScoreCards?.length) {
+            setScoreCards([...scoreCards, ...data.ScoreCards]);
+          }
           if (playerId === 1) {
+            // Receive a card
+            setCards([...cards, data.Card!]);
             setIsCurrentTurn(true);
           } else {
             setIsCurrentTurn(false);
@@ -35,6 +49,8 @@ function App() {
           break;
         case 6: // Player2Turn
           if (playerId === 2) {
+            // Receive a card
+            setCards([...cards, data.Card!]);
             setIsCurrentTurn(true);
           } else {
             setIsCurrentTurn(false);
@@ -53,6 +69,22 @@ function App() {
     <>
       <h1>Player ID: {playerId}</h1>
       <h2>Current turn: {isCurrentTurn ? "Yes" : "No"}</h2>
+      <h2>Won Cards:</h2>
+      <ul>
+        {scoreCards.map((card, index) => (
+          <li key={index}>
+            {card.Value} of {card.Suit}
+          </li>
+        ))}
+      </ul>
+      <h2>Drawn cards:</h2>
+      <ul>
+        {cards.map((card, index) => (
+          <li key={index}>
+            {card.Value} of {card.Suit}
+          </li>
+        ))}
+      </ul>
       <button
         onClick={() => {
           if (isCurrentTurn) {
